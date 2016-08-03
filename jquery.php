3db58997acdfe792039515f93f7a56b5c33e3714
gvauditor <geovanni.auditor@gmail.com>
2016-08-03 15:05:07 +0800

    Initial commit

---
+++ jquery.php
@@ -0,0 +1,15 @@
+// this will get the value of the selected item in a radio group named available
+$('input[name="available"]:checked').val(); 
+
+
+// iterate each element
+$('div').each(function() { 
+	alert($(this).html());
+});
+
+/** selecting by attribute value
+ * Select all a tag thas has a title attribute or a value with
+ */
+$('a[title]')
+$('a[title="Progmramming Info"]')
+
